c6f56e67e8702807d7c473adadcebdc0c1100ff2
Mohammadraihan-flow <mohammadraihanhawaldar19@gmail.com>
2025-11-05 09:49:35 +0530

    updated apppy

--- ml/app.py
+++ ml/app.py
@@ -44,5 +44,8 @@ def recommend():
         'track_name': best['track_name']
     })
 
+import os
+
 if __name__ == '__main__':
-    app.run(port=5001, debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
